9f0a4b0eb995afa2b9ed28c2ed9c42be8ef87293
pratibimbsgsits <89031480+pratibimbsgsits@users.noreply.github.com>
2022-03-12 10:05:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const app=express();
 app.get('/', function(req, res) {
     res.sendFile('index.html', {root: __dirname })
 });
+
+app.get('/graffathon', function(req, res) {
+    res.sendFile('new_event.html', {root: __dirname })
+});
